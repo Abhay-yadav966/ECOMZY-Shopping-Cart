014be517dab190539e9b60ca4663268fafa9d240
Abhay Yadav <yadavabhay966@gmail.com>
2024-02-06 11:34:17 +0530

    cart updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             </NavLink>
             <NavLink to="/cart">
               <div className="relative">
-                {/* <FaShoppingCart className="text-2xl" /> */}
+                <FaShoppingCart className="text-2xl" />
                 {
                   cart.length == 0 ? (<span></span>) : (<span className=" absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 flex justify-center items-center animate-bounce rounded-full text-white " >{cart.length}</span>)
                 }
